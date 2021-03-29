2c13a58afbae83e0f17b85dedecb601a98123691
Cristiona KB <cristiona.kadangha@gmail.com>
2021-03-29 21:33:30 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilisateurComponent
   ],
   imports: [
     BrowserModule
